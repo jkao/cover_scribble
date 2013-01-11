e80be4e62cc7dad14143fe478d03acc7727cd823
jkao <jeff.kao.000@gmail.com>
2013-01-11 14:13:49 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoverScribble::Application.config.secret_token = 'f45627b6f2a108083f980870c899a7526b30e6d2ac53f80c480bf05a8bb430ff8974cfe6e76932a3619945e79b240c4efea852bdc37b4f11d19cdb6589d6d5fc'
